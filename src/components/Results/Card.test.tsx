0b3ae076b22a6b7e70f5a1492d5e4bbafbc1f859
Mikita <nck200069@gmail.com>
2026-05-24 13:46:16 +0200

    tests: add Card component unit tests

--- src/components/Results/Card.test.tsx
+++ src/components/Results/Card.test.tsx
@@ -1,25 +1,96 @@
-import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import { act, render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
 import Card from './Card';
 import { store } from '../../store/store.ts';
 import { Provider } from 'react-redux';
 import { BULBASAUR } from '../../constants/testPokemons.ts';
+import {
+  addPokemon,
+  clearAllPokemons,
+  removePokemon,
+} from '../../store/selectedPokemonsSlice.ts';
+import userEvent from '@testing-library/user-event';
 
-const renderTestComponent = () =>
+const renderTestComponent = (cardClickHandler?: VoidFunction) =>
   render(
     <Provider store={store}>
-      <Card pokemon={BULBASAUR} />
+      <Card pokemon={BULBASAUR} onCardClick={cardClickHandler} />
     </Provider>
   );
 
-describe('Card', () => {
-  it('displays the pokemon name', () => {
+describe('Card Component', () => {
+  beforeEach(() => store.dispatch(clearAllPokemons()));
+
+  it('should display pokemon name', () => {
     renderTestComponent();
     expect(screen.getByText('bulbasaur')).toBeInTheDocument();
   });
 
-  it('displays the pokemon description', () => {
+  it('should display pokemon description', () => {
     renderTestComponent();
     expect(screen.getByText('Grass, Poison')).toBeInTheDocument();
+  });
+
+  it('should have unchecked checkbox by default', () => {
+    renderTestComponent();
+
+    expect(screen.getByRole('checkbox')).not.toBeChecked();
+  });
+
+  it('should have checked checkbox when pokemon is selected in store', () => {
+    store.dispatch(addPokemon(BULBASAUR));
+    renderTestComponent();
+
+    expect(screen.getByRole('checkbox')).toBeChecked();
+  });
+
+  it('should dispatch addPokemon and removePokemon on checkbox clicks', () => {
+    const dispatchSpy = vi.spyOn(store, 'dispatch');
+    renderTestComponent();
+    const checkbox = screen.getByRole('checkbox');
+
+    act(() => {
+      checkbox.click();
+    });
+    expect(dispatchSpy).toHaveBeenCalledWith(addPokemon(BULBASAUR));
+
+    act(() => {
+      checkbox.click();
+    });
+    expect(dispatchSpy).toHaveBeenCalledWith(removePokemon(BULBASAUR.id));
+  });
+
+  it('should not call onCardClick when checkbox is clicked', () => {
+    const onCardClick = vi.fn();
+    renderTestComponent(onCardClick);
+
+    act(() => {
+      screen.getByRole('checkbox').click();
+    });
+
+    expect(onCardClick).not.toHaveBeenCalled();
+  });
+
+  it('should call onCardClick with pokemon id when card is clicked', () => {
+    const onCardClick = vi.fn();
+    renderTestComponent(onCardClick);
+
+    act(() => {
+      screen.getByRole('button').click();
+    });
+
+    expect(onCardClick).toHaveBeenCalledWith(BULBASAUR.id);
+  });
+
+  it('should call onCardClick with pokemon id when "enter button" pressed', async () => {
+    const onCardClick = vi.fn();
+    renderTestComponent(onCardClick);
+
+    act(() => {
+      screen.getByRole('button').focus();
+    });
+    await userEvent.keyboard('{Enter}');
+
+    expect(onCardClick).toHaveBeenCalledWith(BULBASAUR.id);
   });
 });

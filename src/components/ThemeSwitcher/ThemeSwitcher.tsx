e76bc308d66c1175e6e70075eda806838a4a38ce
Mikita <nck200069@gmail.com>
2026-05-24 15:27:31 +0200

    feat|fix: save theme to localstorage & fix switcher animation

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useEffect, useRef } from 'react';
 import { useTheme } from '../../context/useTheme.ts';
 import classes from './ThemeSwitcher.module.css';
 import moonIcon from '../../assets/moon.svg';
@@ -6,7 +6,12 @@ import sunIcon from '../../assets/sun2.svg';
 import clsx from 'clsx';
 
 const ThemeSwitcher = memo(() => {
+  const isMounted = useRef(false);
   const { toggleTheme, isDarkMode } = useTheme();
+
+  useEffect(() => {
+    isMounted.current = true;
+  }, []);
 
   return (
     <button className={classes.switcher} onClick={toggleTheme}>
@@ -15,7 +20,12 @@ const ThemeSwitcher = memo(() => {
       <div
         className={clsx(
           classes.activeIndicator,
-          isDarkMode ? classes.slideLeft : classes.slideRight
+          !isDarkMode ? classes.leftPosition : classes.rightPosition,
+          isMounted.current
+            ? isDarkMode
+              ? classes.slideRight
+              : classes.slideLeft
+            : undefined
         )}
       ></div>
     </button>
--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,18 +1,23 @@
 import { type PropsWithChildren, useEffect, useState } from 'react';
 import { ThemeContext } from './ThemeContext';
+import { THEME_KEY } from '../constants/storage.ts';
 
 export const ThemeProvider = ({ children }: PropsWithChildren) => {
-  const [isDarkMode, setDarkMode] = useState(true);
+  const [isDarkMode, setDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem(THEME_KEY);
+
+    return savedTheme === 'dark';
+  });
 
   const toggleTheme = () => {
     setDarkMode((mode) => !mode);
   };
 
   useEffect(() => {
-    document.documentElement.setAttribute(
-      'data-theme',
-      isDarkMode ? 'dark' : 'light'
-    );
+    const theme = isDarkMode ? 'dark' : 'light';
+
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem(THEME_KEY, theme);
   }, [isDarkMode]);
 
   return (

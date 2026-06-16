25ba8810e8d5a88b9020a87e464048691f7ca7e3
Mikita <nck200069@gmail.com>
2026-06-16 17:54:33 +0200

    fix: resolve thread

--- src/utils/getErrorMessage.ts
+++ src/utils/getErrorMessage.ts
@@ -1,14 +1,18 @@
-import type {FetchBaseQueryError} from "@reduxjs/toolkit/query";
-import type {SerializedError} from "@reduxjs/toolkit";
+import type { FetchBaseQueryError } from '@reduxjs/toolkit/query';
+import type { SerializedError } from '@reduxjs/toolkit';
 
-export const getErrorMessage = (error: FetchBaseQueryError | SerializedError) => {
-	if("status" in error){
-		return String(error.data)
-	}
+export const getErrorMessage = (
+  error: FetchBaseQueryError | SerializedError
+) => {
+  if ('status' in error) {
+    return typeof error.data === 'string'
+      ? error.data
+      : JSON.stringify(error.data);
+  }
 
-	if(error.message){
-		return error.message;
-	}
+  if (error.message) {
+    return error.message;
+  }
 
-	return "Pokemon not found"
-}
+  return 'Pokemon not found';
+};
